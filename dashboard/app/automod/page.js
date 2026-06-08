fbc12fc6be2f955f640ad9f06ea4fbcfb847f3fb
MrTacoL <106849949+MrTacoL@users.noreply.github.com>
2026-06-08 00:37:34 -0400

    Add automod page from Larix module config

---
+++ dashboard/app/automod/page.js
@@ -0,0 +1,6 @@
+import ModulePage from '../components/ModulePage';
+import { modules } from '../config/modules';
+
+export default function AutomodPage() {
+  return <ModulePage module={modules.automod} />;
+}
